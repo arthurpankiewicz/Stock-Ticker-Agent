ed821e6dd102ba547632c19ac522e00f1f06335b
Jacky Lui <jackycodes@gmail.com>
2016-02-10 12:49:31 -0800

    updated UI

--- application/views/home/players_table.php
+++ application/views/home/players_table.php
@@ -1,4 +1,4 @@
-<table class="table">
+<table class="table table-bordered">
     <tr>
         <th>Player</th>
         <th>Cash</th>
--- application/views/home/stocks_table.php
+++ application/views/home/stocks_table.php
@@ -1,4 +1,4 @@
-<table class="table">
+<table class="table table-bordered">
     <tr>
         <th>Code</th>
         <th>Name</th>
